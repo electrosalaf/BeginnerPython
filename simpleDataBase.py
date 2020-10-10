02fbe9d2706aae9f7448819874783d6e0afec1a2
Ibrahim Lawal <lawalibrahimayodeji@gmail.com>
2020-10-10 21:45:00 +0100

    Beginning Python from Novice to Professional Text

---
+++ simpleDataBase.py
@@ -0,0 +1,25 @@
+# A simple database
+
+# A dictionary with person names as keys. Each person is represented as
+# another dictionary with the keys 'phone' and 'addr' referring to their phone
+# number and address, respectively.
+
+people = {
+    
+    'Abdullah': {
+        'phone': '0903605XXXX',
+        'city': 'Ede'
+    },
+   'Ibrahim': {
+        'phone': '0906020XXX',
+        'city': 'Ikirun'
+    },
+   'Yuusuf': {
+        'phone': '0816087XXXX',
+        'city': 'Ogbomosho'
+    }
+   
+}
+
+# mDescriptive label for the phone number amd the address. This will be used 
+# when pritting the output
